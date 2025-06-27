cb123a4547e7560df8986b327b6f93fb33aaf73a
Marcello Menezes <marcello.eam@gmail.com>
2025-06-27 15:11:13 -0300

    escolha entre treinamento e teste + epsilon-greedy

--- client.py
+++ client.py
@@ -11,42 +11,73 @@ q_matrix = np.zeros((96, 3))  # Matriz Q inicializada com zeros
 N_matrix = np.zeros((96, 3))  # Matriz de contagem de ações
 actions = ["left", "right", "jump"] # Lista de ações possíveis
 
-alpha = 0.1  # Taxa de aprendizado (0.2 é o valor do exemplo no notebook)
+alpha = 0.5  # Taxa de aprendizado (0.2 é o valor do exemplo no notebook)
 gamma = 0.9  # Fator de desconto (0.5 é o valor do exemplo no notebook)
 
-T = 2500  # Número de trajetórias a serem executadas
+epsilon = 1.0
+e_decay = 0.9995
+e_min = 0.01
+
+T = 10000  # Número de trajetórias a serem executadas
 
 def q_update(state, action, next_state, reward, q_matrix, alpha, gamma):
     estimate_q = reward + gamma * np.max(q_matrix[next_state, :])
-    q_value = q_matrix[state,action] + alpha * (estimate_q - q_matrix[state][action])
+    q_value = q_matrix[state, action] + alpha * (estimate_q - q_matrix[state, action])
     return q_value
 
-for i in range(T):
+def choose_action(state):
+    if np.random.uniform(0, 1) < epsilon:
+        return actions[np.random.choice([0, 1, 2])]
+    else:
+        return actions[np.argmax(q_matrix[state, :])]
 
-    print(f"Trajetoria {i}")
+mode = input("1: Treinamento, 2: Teste\n")
+
+if(int(mode) == 1):
+    for i in range(T):
+        print(f"Trajetoria {i}, Epsilon {epsilon}")
+        state = 0
+        terminal = True
+        while terminal:
+            action =  choose_action(state) # Escolhe uma ação aleatória
+
+            N_matrix[state][actions.index(action)] += 1  # Incrementa a contagem de ações
+
+            # Envia a ação e recebe o novo estado e recompensa
+            
+            next_state, reward = cn.get_state_reward(s, action)
+            next_state = int(next_state[2:], 2)
+
+            q_matrix[state][actions.index(action)] = q_update(state, actions.index(action), next_state, reward, q_matrix, alpha, gamma)
+            
+            state = next_state
+
+            if reward == 300 or reward == -100:
+                terminal = False
+        epsilon = max(e_min, epsilon * e_decay)
+    # criar arquivo de resultados
+    with open("result.txt", "w") as f:
+        for row in q_matrix:
+            f.write(f" ".join(map(lambda x: f"{x:.6f}", row)) + "\n")
+    # criar arquivo de ações
+    with open("actions.txt", "w") as f:
+        for row in N_matrix:
+            f.write(f" ".join(map(str, row)) + "\n")
+elif (int(mode) == 2):
+    q_matrix = np.loadtxt("result.txt", delimiter=" ")
     terminal = True
     state = 0
     while terminal:
-        action = actions[np.random.choice([0, 1, 2])]  # Escolhe uma ação aleatória
-
-        N_matrix[state][actions.index(action)] += 1  # Incrementa a contagem de ações
-
-        # Envia a ação e recebe o novo estado e recompensa
+        action = actions[np.argmax(q_matrix[state, :])]
         next_state, reward = cn.get_state_reward(s, action)
         next_state = int(next_state[2:], 2)
-
-        q_matrix[next_state][actions.index(action)] = q_update(state, actions.index(action), next_state, reward, q_matrix, alpha, gamma)
         print(f"Ação: {action}, Estado: {next_state}, Recompensa: {reward}")
-        
         state = next_state
-
         if reward == 300 or reward == -100:
             terminal = False
-
-# criar arquivo de resultados
-with open("result.txt", "w") as f:
-    for row in q_matrix:
-        f.write(f" ".join(map(lambda x: f"{x:.6f}", row)) + "\n")
+else:
+    input("Unexpected input. Press enter to leave.")
+    
 
 # for i in range (T):
 #     state, reward = cn.get_state_reward(s, "jump")
